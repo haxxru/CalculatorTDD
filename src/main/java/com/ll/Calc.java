b1becef07afbdf76d74727110f7d86a46d5834b3
Unordinary <92520481+Unord1nary@users.noreply.github.com>
2026-04-20 16:36:14 +0900

    Feat : t5~t7 뺄셈 계산 통과

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,12 +2,16 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+        String operator = expression.contains(" + ") ? "+" : "-";
+        String[] expressionBits = expression.split(" \\" + operator + " ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
         int num2 = Integer.parseInt(expressionBits[1]);
 
-        return num1 + num2;
-    }
+        if (operator.equals("+")) {
+            return num1 + num2;
+        }
 
+        return num1 - num2;
+    }
 }
